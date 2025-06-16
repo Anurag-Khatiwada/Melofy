73e6ec032a6b6e2a0a97401d43498fdfe366355d
Anurag-Khatiwada <anuragkhatiwada108@gmail.com>
2025-06-16 18:07:32 +0545

    frontend started

---
+++ frontend/src/components/Playlist/PlayListCard.tsx
@@ -0,0 +1,16 @@
+import {FaMusic} from "react-icons/fa"
+const PlayListCard = () => {
+  return (
+    <div className="flex items-center p-4 rounded-lg shadow-mg cursor-pointer hover:bg-[#ffff1626]">
+      <div className="w-10 h-10 bg-grey-600 felx items-center justify-center rounded-md ">
+        <FaMusic className="text-white text-xl "/>
+      </div>
+      <div className="ml-4 ">
+        <h2>My PlayList</h2>
+        <p className="text-gray-400 text-sm">PlayList • <span>{"User"}</span> </p>
+      </div>
+    </div>
+  )
+}
+
+export default PlayListCard
